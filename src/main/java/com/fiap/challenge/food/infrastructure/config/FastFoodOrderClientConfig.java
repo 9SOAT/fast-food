3fc96c3b39f440b7fd1654fcbb1f442b03a253b8
Rafael Oliveira <rafaelolivr16@gmail.com>
2025-05-14 23:06:37 -0300

    remove: remoção do order domain e integração fast_food_order

---
+++ src/main/java/com/fiap/challenge/food/infrastructure/config/FastFoodOrderClientConfig.java
@@ -0,0 +1,43 @@
+package com.fiap.challenge.food.infrastructure.config;
+
+import com.fiap.challenge.food.infrastructure.integration.FastFoodOrderClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class FastFoodOrderClientConfig {
+
+
+    @Value("${app.integration.order.base-url}")
+    private String orderBaseUrl;
+
+    @Value("${app.integration.order.timeout:5000}")
+    private long timeoutMillis;
+
+    @Bean
+    public RestClient OrderRestClient() {
+        return RestClient.builder()
+            .baseUrl(orderBaseUrl)
+            .defaultHeader("Content-Type", "application/json")
+            .build();
+
+    }
+
+    @Bean
+    public FastFoodOrderClient fastFoodOrderClient(RestClient restClient) {
+        // Criando um adaptador do RestClient
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
+
+        // Configurando o HttpServiceProxyFactory com o adaptador
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter)
+//            .blockTimeout(Duration.ofMillis(timeoutMillis))
+            .build();
+
+        // Criando o proxy que implementa a interface FastFoodOrderClient
+        return factory.createClient(FastFoodOrderClient.class);
+    }
+}
