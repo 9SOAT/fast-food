3376d7408a6dd2ef13edcf0828a3d807217ac2f0
Victor Ambiel <vambiel@fullstacklabs.co>
2025-05-19 20:40:14 -0300

    feat: adiciona logs do request para o order

--- src/main/java/com/fiap/challenge/food/infrastructure/repository/ExternalOrderRepository.java
+++ src/main/java/com/fiap/challenge/food/infrastructure/repository/ExternalOrderRepository.java
@@ -2,11 +2,14 @@ package com.fiap.challenge.food.infrastructure.repository;
 
 import com.fiap.challenge.food.domain.model.order.Order;
 import com.fiap.challenge.food.domain.ports.outbound.OrderRepository;
+import com.fiap.challenge.food.infrastructure.entity.OrderEntity;
 import com.fiap.challenge.food.infrastructure.integration.FastFoodOrderClient;
 import com.fiap.challenge.food.infrastructure.mapper.EntityMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class ExternalOrderRepository implements OrderRepository {
 
     private final FastFoodOrderClient fastFoodOrderClient;
@@ -19,7 +22,10 @@ public class ExternalOrderRepository implements OrderRepository {
 
     @Override
     public Order save(Order order) {
-        return entityMapper.toOrder(fastFoodOrderClient.saveOrder(order));
+        OrderEntity orderEntity = fastFoodOrderClient.saveOrder(order);
+        log.info("Order saved: {}", orderEntity.getId());
+
+        return entityMapper.toOrder(orderEntity);
     }
 
     @Override
